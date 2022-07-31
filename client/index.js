e1e416b1e529051046c5f045e130edd1f92ba846
Ridercogswell <ridercogswell@gmail.com>
2022-07-30 19:50:06 -0700

    show up

--- client/index.js
+++ client/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
+import {ApolloClient} from '@apollo/client';
+import { ApolloProvider, ApolloCache } from '@apollo/client';
 
-const client = new ApolloClient({});
+const client = new ApolloClient({
+  cache: ApolloCache
+});
 const Root = () => {
   return (
     <ApolloProvider client={client}> 
